6abd099de2a9f522de3dd770f5c76a086c8df154
Jinlong06 <salonvv0616@gmail.com>
2025-12-06 19:39:56 +0800

    Update SelectBubbleInsert.java
    备注代码逻辑

--- src/class004/SelectBubbleInsert.java
+++ src/class004/SelectBubbleInsert.java
@@ -14,6 +14,8 @@ public class SelectBubbleInsert {
 		if (arr == null || arr.length < 2) {
 			return;
 		}
+		// 思路： 在0-N-1范围内，两两比较,将最小的放在i位置
+        // 外层划定了每次循环比较的起始位置或结束位置。选择排序是每次终点位置固定，起始位置在变。
 		for (int minIndex, i = 0; i < arr.length - 1; i++) {
 			minIndex = i;
 			for (int j = i + 1; j < arr.length; j++) {
@@ -30,6 +32,8 @@ public class SelectBubbleInsert {
 		if (arr == null || arr.length < 2) {
 			return;
 		}
+		// 思路: 每次循环，最后一个位置都是最大值，过程中两两比较
+        // 外层划定了每次循环比较的起始位置或结束位置。冒泡排序是每次起始位置固定，终止位置在变。
 		for (int end = arr.length - 1; end > 0; end--) {
 			for (int i = 0; i < end; i++) {
 				if (arr[i] > arr[i + 1]) {
@@ -44,6 +48,8 @@ public class SelectBubbleInsert {
 		if (arr == null || arr.length < 2) {
 			return;
 		}
+		// 思路: 每次循环，0~N-1默认已经有序，新数在i位置，向左进行比较
+        // 外层划定了每次循环比较的起始位置或结束位置。插入排序是终止起始位置固定，起始位置在变。
 		for (int i = 1; i < arr.length; i++) {
 			for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
 				swap(arr, j, j + 1);
